e7fd879a1250d8ce43b69ac99771a37d46eab911
Faikar <ghassanfaikar@yahoo.com>
2021-12-03 15:45:43 +0700

    Add files via upload

---
+++ resources/views/web/term/index.blade.php
@@ -0,0 +1,121 @@
+@extends('components/layout.index')
+@section('header')
+    <header page="profile">
+        <menu>
+            <div class="container-fluid g-0">
+                @include('components/presentational/menu/index')
+            </div>
+        </menu>
+    </header>
+@endsection
+@section('main')
+    <main page="term">
+        <section class="section__first">
+            <div class="container-fluid g-0">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Kebijakan Privasi</h1>
+                        <p>Kebijakan Privasi ini adalah komitmen nyata dari PT Bintang Toedjoe (selanjutnya disebut sebagai “BINA”) untuk menghargai dan melindungi setiap data dan/atau informasi pribadi Pengguna situs https://[ ], situs-situs turunannya, dan aplikasi gawainya (selanjutnya disebut sebagai “Situs/Aplikasi”). Kebijakan Privasi ini merupakan satu kesatuan dan bagian yang tidak terpisahkan dari Syarat dan Ketentuan sebagaimana dimaksud dalam Situs/Aplikasi.</p>
+                        <p>Kebijakan Privasi, Syarat dan Ketentuan, dan informasi lain yang tercantum pada Situs/Aplikasi mengatur tentang perolehan, pengumpulan, pengolahan, penganalisaan, penampilan, pembukaan dan/atau segala bentuk pengelolaan yang terkait dengan data dan/atau informasi yang Pengguna berikan kepada BINA dan/atau data dan/atau informasi yang BINA peroleh dari Pengguna. Data dan/atau informasi tersebut meliputi data pribadi Pengguna yang BINA peroleh pada saat Pengguna melakukan pendaftaran pada Situs/Aplikasi, Pengguna mengakses Situs/Aplikasi, dan Pengguna menggunakan layanan-layanan yang tersedia pada Situs/Aplikasi (selanjutnya disebut sebagai “Data”).</p>
+                        <p>Dengan mendaftar, mengakses dan/atau menggunakan layanan-layanan yang tersedia pada Situs/Aplikasi, Pengguna menyatakan dan menjamin bahwa setiap Data adalah benar dan sah, dan Pengguna dengan ini memberikan persetujuan kepada BINA untuk memperoleh, mengumpulkan, menyimpan, mengelola, dan menggunakan seluruh Data tersebut.</p>
+                        <br>
+                        <h2>A. Perolehan dan Pengumpulan Data</h2>
+                        <p>Tujuan BINA mengumpulkan Data adalah untuk menyediakan layanan bagi Pengguna Situs/Aplikasi, termasuk namun tidak terbatas pada memproses transaksi Pengguna, mengelola dan memperlancar penggunaan Situs/Aplikasi, dan tujuan-tujuan lain yang diizinkan oleh peraturan perundang-undangan yang berlaku. Data yang dikumpulkan oleh BINA meliputi:</p>
+                        <p>1.   Data yang diserahkan secara mandiri oleh Pengguna, termasuk namun tidak terbatas pada Data yang diserahkan pada saat Pengguna:</p>
+                        <ol type="a">
+                            <li>membuat atau memperbaharui akun, termasuk namun tidak terbatas pada nama, alamat e-mail, nomor telepon atau handphone, password, alamat, foto, dan/atau informasi lainnya;</li>
+                            <li>menghubungi BINA, termasuk namun tidak terbatas pada media Layanan Pengguna;</li>
+                            <li>menggunakan layanan-layanan yang tersedia pada Situs/Aplikasi, termasuk data transaksi yang detail, diantaranya jenis, jumlah dan/atau keterangan dari produk yang dibeli, alamat pengiriman, kanal pembayaran yang digunakan, jumlah transaksi, tanggal dan waktu transaksi, serta detail transaksi lainnya;</li>
+                            <li>menggunakan fitur yang membutuhkan izin akses terhadap perangkat Pengguna; dan/atau</li>
+                            <li>Data lain yang diserahkan melalui layanan pihak ketiga, sehubungan dengan penggunaan Situs/Aplikasi.</li>
+                        </ol>
+                        <p>2.   Data yang terekam pada saat Pengguna menggunakan Situs/Aplikasi, termasuk namun tidak terbatas pada:</p>
+                        <ol type="a">
+                            <li>data lokasi riil atau perkiraannya, termasuk namun tidak terbatas pada alamat IP (Internet Protocol), lokasi Wi-Fi, geo-location, dan sebagainya;</li>
+                            <li>data berupa waktu dari setiap aktivitas Pengguna, termasuk namun tidak terbatas pada aktivitas pendaftaran, login, penggunaan layanan Situs/Aplikasi, dan sebagainya;</li>
+                            <li>data penggunaan atau preferensi Pengguna, diantaranya interaksi Pengguna dalam menggunakan Situs/Aplikasi, pilihan yang disimpan, serta pengaturan yang dipilih. Data tersebut diperoleh menggunakan, termasuk namun tidak terbatas pada cookies, pixel tags, dan/atau teknologi serupa yang menciptakan dan mempertahankan pengenal unik;</li>
+                            <li>data perangkat, diantaranya jenis perangkat yang digunakan untuk mengakses Situs/Aplikasi, termasuk namun tidak terbatas pada model perangkat keras, sistem operasi dan versinya, perangkat lunak, nama file dan versinya, pilihan bahasa, pengenal perangkat unik, pengenal iklan, nomor seri, informasi gerakan perangkat dan/atau informasi jaringan seluler, dan data tambahan lainnya yang dibutuhkan sehubungan dengan permintaan akses peramban atau aplikasi pada perangkat Pengguna;</li>
+                            <li>data catatan (log), diantaranya catatan pada server yang menerima Data seperti alamat IP perangkat, tanggal dan waktu akses, fitur aplikasi atau laman yang dilihat, proses kerja aplikasi dan aktivitas sistem lainnya, jenis peramban, dan/atau situs atau layanan pihak ketiga yang Pengguna gunakan sebelum berinteraksi dengan Situs/Aplikasi.</li>
+                        </ol>
+                        <br>
+                        <h2>B. Penggunaan Data</h2>
+                        <p>BINA dapat menggunakan seluruh atau sebagian Data yang diperoleh dan dikumpulkan dari Pengguna sebagaimana dimaksud dalam bagian sebelumnya untuk hal-hal sebagai berikut:</p>
+                        <ol>
+                            <li>memproses segala bentuk permintaan, pengajuan, aktivitas yang dilakukan oleh Pengguna melalui Situs/Aplikasi;</li>
+                            <li>menyediakan fitur-fitur untuk memberikan, mewujudkan, memelihara dan/atau memperbaiki produk dan layanan, termasuk namun tidak terbatas pada melakukan kegiatan internal yang diperlukan untuk menyediakan layanan pada Situs/Aplikasi, termasuk namun tidak terbatas pada pemecahan masalah software, bug, permasalahan operasional, melakukan analisis Data, pengujian, penelitian, dan/atau untuk memantau dan menganalisis kecenderungan penggunaan dan aktivitas dari Pengguna;</li>
+                            <li>membantu Pengguna pada saat berkomunikasi menggunakan Layanan Pengguna melalui Situs/Aplikasi, diantaranya untuk:</li>
+                            <ol type="a">
+                                <li>memeriksa dan mengatasi kendala/permasalahan Pengguna;</li>
+                                <li>mengawasi dan memperbaiki tanggapan Layanan Pengguna;</li>
+                            </ol>
+                            <li>menggunakan Data untuk tujuan penelitian, analisis, pengembangan dan/atau pengujian guna meningkatkan keamanan dan kenyamanan layanan-layanan pada Situs/Aplikasi, serta mengembangkan fitur dan/atau layanan baru;</li>
+                            <li>elakukan pemantauan/investigasi terhadap kegiatan, aktivitas dan/atau transaksi yang mencurigakan dan/atau terindikasi mengandung unsur kecurangan atau pelanggaran terhadap Syarat dan Ketentuan atau peraturan perundang-undangan yang berlaku, serta melakukan tindakan-tindakan yang diperlukan sebagai tindak lanjut dari hasil pemantauan/investigasi terhadap kegiatan, aktivitas dan/atau transaksi tersebut.</li>
+                            <li>BINA dapat menggunakan dan/atau mengungkapkan Data untuk tujuan penegakan hukum, termasuk namun tidak terbatas pada sengketa yang terjadi diantara Pengguna dengan BINA.</li>
+                        </ol>
+                        <br>
+                        <h2>C. Pengungkapan Data</h2>
+                        <p>BINA dengan ini menyatakan dan menjamin bahwa BINA tidak akan menjual, mengalihkan, mendistribusikan dan/atau meminjamkan Data kepada pihak manapun tanpa memperoleh izin terlebih dahulu dari Pengguna, kecuali dalam hal:</p>
+                        <ol>
+                            <li>dibutuhkan adanya pengungkapan Data kepada pihak ketiga yang membantu BINA dalam menyajikan layanan pada Situs/Aplikasi dan memproses segala bentuk aktivitas Pengguna dalam Situs/Aplikasi;</li>
+                            <li>BINA dapat menyediakan Data yang relevan kepada vendor, konsultan hukum, konsultan keuangan, auditor eksternal, mitra pemasaran, firma riset, penyedia layanan sejenis dan/atau pihak ketiga lainnya, termasuk namun tidak terbatas pada kegiatan pemasaran yang dilakukan oleh pihak ketiga;</li>
+                            <li>Pengguna menghubungi BINA melalui media publik seperti blog, media sosial dan/atau fitur tertentu pada Situs/Aplikasi, dimana komunikasi antara Pengguna dan BINA mungkin dapat dilihat oleh publik;</li>
+                            <li>BINA dapat membagikan Data kepada anak perusahaan dan/atau afiliasinya untuk membantu menyediakan layanan atau melakukan pengolahan Data untuk dan atas nama BINA;</li>
+                            <li>BINA mengungkapkan Data dalam upaya mematuhi kewajiban hukum dan/atau adanya permintaan yang sah dari aparat penegak hukum dan/atau instansi pemerintah yang berwenang.</li>
+                        </ol>
+                        <br>
+                        <h2>D. Cookies</h2>
+                        <ol>
+                            <li>Cookies adalah file kecil yang secara otomatis akan mengambil tempat di dalam perangkat Pengguna yang menjalankan fungsi dalam menyimpan preferensi maupun konfigurasi Pengguna selama mengunjungi suatu situs.</li>
+                            <li>Pengguna dapat menentukan pilihan untuk menerima, menolak dan/atau menghapus Cookies dalam penggunaan Situs (pilihan ini dapat membatasi layanan optimal pada saat melakukan akses ke situs).</li>
+                        </ol>
+                        <br>
+                        <h2>E. Akses Situs Pihak Ketiga</h2>
+                        <ol>
+                            <li>Situs/Aplikasi dapat memuat pranala atau link menuju situs/aplikasi milik pihak ketiga (selanjutnya disebut sebagai “Situs/Aplikasi Pihak Ketiga”) dan konten pihak ketiga (selanjutnya disebut sebagai “Konten Pihak Ketiga”) yang mungkin menampilkan logo atau merek BINA.</li>
+                            <li>BINA tidak memiliki kendali atas Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga. Oleh karena itu, penggunaan pranala atau link menuju Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga yang dilakukan oleh Pengguna merupakan tanggung jawab Pengguna sepenuhnya.</li>
+                            <li>Ketika Pengguna mengakses dan/atau menggunakan pranala atau link menuju Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga, BINA tidak bertanggung jawab untuk melindungi data dan/atau informasi apapun yang Pengguna berikan. BINA tidak bertanggung jawab atas setiap informasi yang terdapat pada Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga, termasuk namun tidak terbatas pada dampak yang ditimbulkan akibat mengakses Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga.</li>
+                            <li>Pengguna disarankan untuk mempelajari dan membaca dengan saksama kebijakan penanganan informasi pribadi yang berlaku di Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga.</li>
+                            <li>BINA menyediakan pranala atau link menuju Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga hanya untuk kenyamanan Pengguna semata. Hal tersebut tidak dapat ditafsirkan sebagai dukungan, adopsi, sponsor, afiliasi antara BINA dengan Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga.</li>
+                        </ol>
+                        <br>
+                        <h2>E. Akses Situs Pihak Ketiga</h2>
+                        <ol>
+                            <li>Situs/Aplikasi dapat memuat pranala atau link menuju situs/aplikasi milik pihak ketiga (selanjutnya disebut sebagai “Situs/Aplikasi Pihak Ketiga”) dan konten pihak ketiga (selanjutnya disebut sebagai “Konten Pihak Ketiga”) yang mungkin menampilkan logo atau merek BINA.</li>
+                            <li>BINA tidak memiliki kendali atas Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga. Oleh karena itu, penggunaan pranala atau link menuju Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga yang dilakukan oleh Pengguna merupakan tanggung jawab Pengguna sepenuhnya.</li>
+                            <li>Ketika Pengguna mengakses dan/atau menggunakan pranala atau link menuju Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga, BINA tidak bertanggung jawab untuk melindungi data dan/atau informasi apapun yang Pengguna berikan. BINA tidak bertanggung jawab atas setiap informasi yang terdapat pada Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga, termasuk namun tidak terbatas pada dampak yang ditimbulkan akibat mengakses Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga.</li>
+                            <li>Pengguna disarankan untuk mempelajari dan membaca dengan saksama kebijakan penanganan informasi pribadi yang berlaku di Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga.</li>
+                            <li>BINA menyediakan pranala atau link menuju Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga hanya untuk kenyamanan Pengguna semata. Hal tersebut tidak dapat ditafsirkan sebagai dukungan, adopsi, sponsor, afiliasi antara BINA dengan Situs/Aplikasi Pihak Ketiga dan/atau Konten Pihak Ketiga.</li>
+                        </ol>
+                        <br>
+                        <h2>F. Pilihan Pengguna dan Transparansi</h2>
+                        <ol>
+                            <li>Perangkat seluler pada umumnya (iOS, Android, dan sebagainya) memiliki pengaturan sehingga Situs/Aplikasi BINA tidak dapat mengakses Data tertentu tanpa persetujuan dari Pengguna. Perangkat seluler akan memberikan pemberitahuan kepada Pengguna saat aplikasi BINA pertama kali meminta akses terhadap Data tersebut.</li>
+                            <li>Pengguna dapat mengakses dan mengubah informasi berupa alamat e-mail, nomor telepon atau handphone, tanggal lahir, jenis kelamin, dan daftar alamat melalui fitur pengaturan pada Situs/Aplikasi.</li>
+                            <li>Sejauh diizinkan oleh ketentuan yang berlaku, Pengguna dapat menghubungi BINA untuk melakukan penarikan persetujuan terhadap perolehan, pengumpulan, penyimpanan, pengelolaan dan penggunaan Data. Dalam hal tersebut, Pengguna memahami bahwa Pengguna tidak dapat lagi menggunakan layanan yang tersedia pada Situs/Aplikasi.</li>
+                            <li>Apabila terjadi kegagalan dalam perlindungan Data, BINA akan menginformasikan kepada Pengguna dalam bentuk pemberitahuan tertulis yang menyertakan penyebab terjadinya kegagalan perlindungan Data, paling lambat 14 (empat belas) hari kalender terhitung sejak tanggal diketahui terdapat kegagalan perlindungan Data.</li>
+                        </ol>
+                        <br>
+                        <h2>G. Penyimpanan dan Penghapusan Data</h2>
+                        <ol>
+                            <li>BINA akan menyimpan Data selama akun Pengguna aktif.</li>
+                            <li>BINA akan menyimpan Data sampai dengan dilakukannya permintaan penghapusan Data atau penarikan persetujuan terhadap perolehan, pengumpulan, penyimpanan, pengelolaan dan penggunaan Data oleh Pengguna melalui Layanan Pengguna.</li>
+                            <li>BINA akan melakukan penghapusan Data sesuai dengan ketentuan hukum yang berlaku.</li>
+                        </ol>
+                        <br>
+                        <h2>H. Pembaharuan Kebijakan Privasi</h2>
+                        <p>BINA dapat sewaktu-waktu melakukan perubahan atau pembaharuan terhadap Kebijakan Privasi tanpa pemberitahuan terlebih dahulu kepada Pengguna. Pengguna wajib membaca Kebijakan Privasi secara berkala untuk mengetahui perubahan atau pembaharuan yang dilakukan oleh BINA. Dengan tetap mengakses dan/atau menggunakan layanan pada Situs/Aplikasi, maupun layanan BINA lainnya, maka Pengguna dianggap menyetujui setiap perubahan atau pembaharuan yang dilakukan terhadap Kebijakan Privasi ini.</p>
+                        <br>
+                        <h2>I. Lain-lain</h2>
+                        <ol>
+                            <li>Kebijakan Privasi ini dibuat dan diatur dengan tunduk kepada hukum yang berlaku di wilayah Republik Indonesia.</li>
+                            <li>Judul dan Sub-Judul yang terdapat dalam Kebijakan Privasi ini dibuat hanya sebagai acuan dan sama sekali tidak menetapkan, membatasi, menjelaskan atau menjabarkan apa yang tercantum dalam masing-masing bagian.</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
+@section('script')
+
+@endsection
